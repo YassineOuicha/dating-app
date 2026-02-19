00c0ecd9f089ab0bdf97dbd5d7306dd98920d4eb
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2026-02-19 14:54:41 +0100

    Members templates & service

---
+++ client/src/core/services/member-service.ts
@@ -0,0 +1,21 @@
+import {inject, Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Member} from '../../types/member';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberService {
+  private readonly baseUrl = environment.apiUrl;
+  private readonly http = inject(HttpClient);
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'members');
+  }
+
+  getMember(id: string): Observable<Member> {
+    return this.http.get<Member>(this.baseUrl + 'members/' + id);
+  }
+}
--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {MemberService} from '../../../core/services/member-service';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs';
+import {Member} from '../../../types/member';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css',
 })
-export class MemberDetail {
+export class MemberDetail implements OnInit {
+  protected member$?: Observable<Member>;
+  private readonly memberService = inject(MemberService);
+  private readonly route = inject(ActivatedRoute);
 
+  ngOnInit() {
+    this.member$ = this.loadMember();
+  }
+
+  loadMember() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (!id) {
+      return;
+    }
+    return this.memberService.getMember(id);
+  }
 }
